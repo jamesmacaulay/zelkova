3d5f7c87b6bfde96b036613ad186ea9b8ec7c9bb
James MacAulay <james@shopify.com>
2014-11-16 11:51:24 -0500

    alias signal :as z in user ns

--- repl/user.clj
+++ repl/user.clj
@@ -1,7 +1,7 @@
 (ns user
   (:require [jamesmacaulay.async-tools.core :as tools]
             [jamesmacaulay.async-tools.core-test :as tools-test]
-            [jamesmacaulay.zelkova.signal :as signals]
+            [jamesmacaulay.zelkova.signal :as z]
             [jamesmacaulay.zelkova.signal-test :as signals-test]
             [clojure.core.async :as async :refer [go go-loop <! <!! >! >!! chan]]
             [clojure.tools.namespace.repl :refer [refresh]]
