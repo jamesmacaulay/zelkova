829b69e9cb7295f485b0a3a5763596378be7cec9
James MacAulay <james@shopify.com>
2015-02-01 10:54:57 -0500

    update deps

--- project.clj
+++ project.clj
@@ -3,30 +3,28 @@
   :url "http://github.com/jamesmacaulay/zelkova"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
+                 [org.clojure/clojurescript "0.0-2740"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
-  :plugins [[lein-cljsbuild "1.0.3"]
-            [com.keminglabs/cljx "0.4.0" :exclusions [org.clojure/clojure]]
-            [com.cemerick/clojurescript.test "0.3.3"]
-            [com.cemerick/austin "0.1.5"]]
+  :plugins [[lein-cljsbuild "1.0.4"]
+            [com.keminglabs/cljx "0.5.0" :exclusions [org.clojure/clojure]]
+            [com.cemerick/clojurescript.test "0.3.3"]]
   :jar-exclusions [#"\.cljx"]
   :aliases {"repl" ["with-profile" "repl" "repl"]
             "clj-test" ["with-profile" "clj" "test"]
             "cljs-test" ["do" "cljx" ["cljsbuild" "test"]]
             "cljs-autotest" ["do" "cljx" ["cljsbuild" "auto" "test"]]
             "all-tests" ["do" "clean" ["clj-test"] ["cljs-test"]]}
-  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.7"]
-                                  [org.clojure/tools.nrepl "0.2.6"]
-                                  [org.clojure/clojurescript "0.0-2411"]
-                                  [com.cemerick/clojurescript.test "0.3.3"]
-                                  [com.cemerick/austin "0.1.5"]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.9"]
+                                  [org.clojure/tools.nrepl "0.2.7"]
+                                  [com.cemerick/clojurescript.test "0.3.3"]]
                    :source-paths ["target/classes"]}
              :repl [:dev {:source-paths ["repl" "target/classes" "target/generated/test/clj" "target/generated/test/cljs"]
                           :test-paths ["target/generated/test/clj" "target/generated/test/cljs"]}]
              :clj [:dev {:source-paths ["target/classes"]
                          :test-paths ["target/generated/test/clj"]}]
              :cljs [:dev]}
-  :hooks [cljx.hooks]
+  :prep-tasks [["cljx" "once"] "javac" "compile"]
   :cljsbuild {:test-commands {"phantom" ["phantomjs" :runner "target/testable.js"]
                               "node" ["node" :node-runner "target/testable.js"]}
               :builds [{:id "test"
