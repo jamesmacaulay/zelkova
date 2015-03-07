3bc91c95ab75bf7a52544f8015edfe9848e9a305
James MacAulay <james@shopify.com>
2015-03-07 09:08:50 -0500

    rebuild mario example from figwheel template
    lein new figwheel mario -- --om

---
+++ examples/mario/dev_src/mario/dev.cljs
@@ -0,0 +1,10 @@
+(ns mario.dev
+    (:require
+     [mario.core]
+     [figwheel.client :as fw]))
+
+(fw/start {
+  :websocket-url "ws://localhost:3449/figwheel-ws"
+  :on-jsload (fn []
+               ;; (stop-and-start-my app)
+               )})
