1ccdc81ccdd07b589f877ba57bbde3a06bdc8927
James MacAulay <james@shopify.com>
2015-03-22 16:02:19 -0400

    0.3.1-SNAPSHOT

--- examples/drag_and_drop/project.clj
+++ examples/drag_and_drop/project.clj
@@ -7,7 +7,7 @@
   :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
                  [org.clojure/clojurescript "0.0-2850"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [jamesmacaulay/zelkova "0.3.0"]
+                 [jamesmacaulay/zelkova "0.3.1-SNAPSHOT"]
                  [figwheel "0.2.5-SNAPSHOT"]
                  [sablono "0.3.4"]
                  [org.omcljs/om "0.8.8"]]
