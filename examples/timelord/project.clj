812ce7789090d9973aa79b47f99899cf7fb4c1ae
James MacAulay <james@shopify.com>
2015-04-11 15:00:07 -0400

    update deps

--- examples/timelord/project.clj
+++ examples/timelord/project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.7.0-alpha6"]
-                 [org.clojure/clojurescript "0.0-3178"]
+  :dependencies [[org.clojure/clojure "1.7.0-beta1"]
+                 [org.clojure/clojurescript "0.0-3196"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [jamesmacaulay/zelkova "0.3.2-SNAPSHOT"]
                  [figwheel "0.2.5-SNAPSHOT"]
--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://github.com/jamesmacaulay/zelkova"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha6"]
-                 [org.clojure/clojurescript "0.0-3178"]
+  :dependencies [[org.clojure/clojure "1.7.0-beta1"]
+                 [org.clojure/clojurescript "0.0-3196"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
   :plugins [[lein-cljsbuild "1.0.5"]
             [com.keminglabs/cljx "0.6.0" :exclusions [org.clojure/clojure
